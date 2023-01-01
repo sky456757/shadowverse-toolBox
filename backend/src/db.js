c8c3e40f66c6efb930ca0fb8168daddcb76ada8d
Thinker <sky456757@gmail.com>
2023-01-01 14:42:30 +0800

    msg

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv-defaults';
+import dotenv from 'dotenv';
 dotenv.config();
   export default {
     connect: () => { /* code to connect Mongoose DB */ 
