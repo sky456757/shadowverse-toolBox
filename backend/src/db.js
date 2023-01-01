c7461e71e2b58ad4fcaa7eace20954ae09085d5e
Thinker <sky456757@gmail.com>
2023-01-01 14:50:56 +0800

    msg

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
   export default {
     connect: () => { /* code to connect Mongoose DB */ 
+    console.log("start connecting");
     mongoose.connect(process.env.MONGO_URL,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
