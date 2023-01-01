a50b9a8cb1acc22d275b6e61943eff0d7c623d9f
Thinker <sky456757@gmail.com>
2023-01-01 14:33:20 +0800

    msg

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,39 +2,21 @@ import express from 'express'
 import cors from 'cors'
 import routes from './routes'
 import mongoose from 'mongoose'
-require('dotenv').config()
+import bodyParser from 'body-parser';
+import db from './db';
 //console.log(process.env.MONGO_URL) 
+db.connect();
 const app = express()
 
 // init middleware
 app.use(cors())
 app.use(express.json())
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
-    res.header('Access-Control-Allow-Credentials', 'true')
-    next()
-})
-const port = process.env.PORT || 4000
-const dboptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}
-
-// TODO Part I-3: check .env exists
-if (!process.env.MONGO_URL) {
-    console.error("Missing MONGO_URL!!!");
-    process.exit(1);
-}
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,  }
-    // TODO Part I-3: connect the backend to mongoDB
-).then((res) => console.log("mongo db connection created"))
-mongoose.connection.on('error',
-                        console.error.bind(console, 'connection error:'));
+app.use(bodyParser.json());
 routes(app)
+const port = process.env.PORT || 4000
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
 app.listen(port, () => {
     console.log(`Server is up on port ${port}.`)
 })
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:4000/api",
-  });
+  baseURL: "http://localhost:4000/api",
+  //baseURL: "http://shadowverse-tool-box-back.vercel.app"
+});
 
 export default instance;
